c62c8bd06da326aaff02f4fe9a7dc1efdcf880e5
jenny0714 <jennylin0714@gmail.com>
2025-02-09 23:30:32 +0800

    feat: add favicon and static file support
    - Add serve-favicon middleware
    - Configure express.static for public directory
    - Add favicon.ico in public folder
    - Install serve-favicon package

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const favicon = require("serve-favicon");
+
 const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+app.use(express.static("public"));
 
-app.use('/hi',(req, res)=> res.send('Hi!'));
+app.use("/hi", (req, res) => res.send("Hi!"));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
