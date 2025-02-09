b4c997ef3bc5fc7091f6478ffc2ecf872b013868
jenny0714 <jennylin0714@gmail.com>
2025-02-09 22:32:06 +0800

    refactor: migrate to Express framework
    - Replace native http with Express
    - Add /hi endpoint with middleware
    - Update root endpoint to use Express syntax
    - Configure nodemon for development

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.use('/hi',(req, res)=> res.send('Hi!'))
 
-const server = http.createServer((req, res) => {
-    console.log(req.url);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end(`Hello, World!\n ${req.url}`);
-});
+app.get('/', (req, res) => res.send('Hello World!'))
 
-server.listen(port, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
